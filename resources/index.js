552629a8e102c4c6c39bd7516370f16063b4f93e
Mariusmarisca <70143537+Mariusmarisca@users.noreply.github.com>
2020-11-01 15:22:30 +0200

    added files on new branch

--- resources/index.js
+++ resources/index.js
@@ -1,44 +1,54 @@
 document.addEventListener("DOMContentLoaded", function(event){
-    const menuItems = document.getElementsByClassName("menu-item");
-    const menuShop = document.getElementById('menuShop');
-    console.log('menuItems', menuItems)
+    const menuItems = $('.main-menu').find('a');
+    const menuShop = $('.menuShop');
+    console.log('menuShop')
+    console.log('menuItems', menuItems.length)
 
-    for(let i = 0; i < menuItems.length; i++){
-        const menuItem= menuItems[i];
         
-        menuItem.addEventListener('click', function() {
-           for(let j = 0; j < menuItems.length; j++) {
-               if (menuItems[j].classList.contains('selected')){
-                   menuItems[j].classList.remove("selected");
-               } else {
-                   continue;
-               }
-           }
-           menuItem.classList.add("selected");
+        menuItems.click(function(e) {
+               e.preventDefault();
+               console.log('click');
+               menuItems.removeClass('selected');
+               $(this).addClass('selected');
+            
 
-              const dataContent=menuItem.dataset.content;
-              console.log('dataContent', dataContent)
-              const contentElements=document.getElementsByClassName("content");
-              for(let k= 0; k < contentElements.length; k++) {
-                  if(contentElements[k].classList.contains('hidden')) {
-                      continue;
-                  } else {
-                     contentElements[k].classList.add("hidden");
-                  }
-              }
-              document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
+       //la ambele variante nu iese selectia de data-content cum trebuie
+       const dataContent=$('main-menu').attr('data-content');
+       console.log($('main-menu').attr('data-content'));
+       //si nici asa varianta ce am comentat-o mai jos nu iese
+       //const dataCont=$('menu-item).attr('data-content');
+       const contentElements=$('.content')
+        console.log('content:', contentElements.length);
+        contentElements.each(function(index,item) {
+           contentElements.addClass('hidden');
+            $(this).removeClass('hidden');
+            
+       } 
+            )
+              //const dataContent=menuItems.dataset.content;
+              //console.log('dataContent', dataContent);
+             //const contentElements=document.getElementsByClassName("content");
+              //for(let i= 0; i < contentElements.length; i++) {
+                //if(contentElements[i].classList.contains('hidden')){
+                  //  continue;
+               // } else {
+                //    contentElements[i].classList.add("hidden");
+              //  }
+            //}
+           // document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
+             
               
               //o idee de cum poti sa ascunzi un element conditional
-              if(dataContent === 'orders-container') {
+              //if(dataContent === 'orders-container') {
                   //menuShop e selectat mai sus oe randul 3
-                  menuShop.classList.add('hidden');
-              } else {
-                menuShop.classList.remove('hidden');
-              }
+                //menuShop.classList.add('hidden');
+              //} else {
+                 //menuShop.classList.remove('hidden');
+              //} 
 
               const dataLogo=menuItem.dataset.logo;
               console.log('dataLogo', dataLogo)
-              const logoElements=document.getElementsByClassName("logo");
+              const logoElements= $('.logo')//document.getElementsByClassName("logo");
               for(let m= 0; m < logoElements.length; m++) {
                   if(logoElements[m].classList.contains('hidden')){
                       continue;
@@ -47,24 +57,29 @@ document.addEventListener("DOMContentLoaded", function(event){
                   }
               }
               document.getElementsByClassName(dataLogo)[0].classList.remove("hidden");
-        }
+            }
         )
-    }
-        const ordersLink= document.getElementById("order-link");
+
+          ordersLink=document.getElementById("order-link");
         console.log('ordersLink', ordersLink)
-        
-           ordersLink.addEventListener('click', function() {
-               const dataLink=ordersLink.dataset.content
-               console.log('dataLink', dataLink)
-               const linkElements=document.getElementsByClassName("content");
-               for(let n= 0; n< linkElements.length; n++) {
-                   if(linkElements[n].classList.contains('hidden')) {
-                       continue;
-                   } else {
-                       linkElements[n].classList.add("hidden");
-                   }
-               }
-               document.getElementsByClassName(dataLink)[0].classList.remove("hidden");
+         
+                         $('#order-link').click(function () {
+                      const dataLink=$('.linked-page');
+                      console.log('dataLink:', dataLink) 
+                      $(this).removeClass('hidden');
+                       $('content').addClass('hidden');
+                         
+               //const dataLink=ordersLink.dataset.content
+               //console.log('dataLink', dataLink)
+               //const linkElements=document.getElementsByClassName("content");
+               //for(let n= 0; n< linkElements.length; n++) {
+                  // if(linkElements[n].classList.contains('hidden')) {
+                    //   continue;
+                   //} else {
+                   //    linkElements[n].classList.add("hidden");
+                   // }
+               
+              // document.getElementsByClassName(dataLink)[0].classList.remove("hidden");
            }
            )
 }
